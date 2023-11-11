9eab36f5868a33fab84716827d3a2ba59fd5dc4d
korostelv <valenok2025@gmail.com>
2023-11-11 16:08:35 +0500

    исправление карты

--- TravelBlog/static/js/map.js
+++ TravelBlog/static/js/map.js
@@ -7,39 +7,38 @@ $(document).ready(function(){
     let cityString = $('#user_city').text();
     let cityDict = JSON.parse(cityString);
 
-    console.log($('.title h3').text())
 
 
-    // let map = L.map('map').setView([58.00, 56.15], 6); //По координатам  Перми
-    let map = L.map('map').setView(loc, 6); // По координатам по IP
+    ////////////////////////////////////////////////////////////////////////
+    
+    // //let map = L.map('map').setView([58.00, 56.15], 6); //По координатам  Перми
+    // let map = L.map('map').setView(loc, 6); // По координатам по IP
 
 
 
-    const basemaps = {
-        StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: '&copy; OpenStreetMap'}),
-        Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
-        Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'}),
-    };
-    basemaps.StreetView.addTo(map);
-    L.control.layers(basemaps).addTo(map);
+    // const basemaps = {
+    //     StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: '&copy; OpenStreetMap'}),
+    //     Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
+    //     Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'}),
+    // };
+    // basemaps.StreetView.addTo(map);
+    // L.control.layers(basemaps).addTo(map);
+
+    // const flagIcon = L.icon({
+    //     iconUrl: 'https://raw.githubusercontent.com/korostelv/image/main/flag.png',
+    //     iconSize: [40, 40],
+    // });
 
 
-  
-
-    const flagIcon = L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/korostelv/image/main/flag.png',
-        iconSize: [40, 40],
-    });
+    // // L.marker([58.00, 56.15]).bindPopup('Вы находитесь здесь.').addTo(map);  //Метка местоположения Перми
+    // L.marker(loc).bindPopup('Вы находитесь здесь.').addTo(map); //метка местоположения по IP
 
 
-    // L.marker([58.00, 56.15]).bindPopup('Вы находитесь здесь.').addTo(map);  //Метка местоположения Перми
-    L.marker(loc).bindPopup('Вы находитесь здесь.').addTo(map); //метка местоположения по IP
+    // for ( let i in cityDict) {
+    //     L.marker([cityDict[i]['lat'], cityDict[i]['long']], {icon: flagIcon}).bindPopup(i).addTo(map);
+    // }
 
-
-    for ( let i in cityDict) {
-        L.marker([cityDict[i]['lat'], cityDict[i]['long']], {icon: flagIcon}).bindPopup(i).addTo(map);
-    }
-
+///////////////////////////////////////////////////////////////////////////////////////
 
 //  Геолокация в JS определяется только при защищенном соединении
     // function getCurrentLocation() {
@@ -84,6 +83,79 @@ $(document).ready(function(){
     //     .catch(error => {
     //         console.error("Ошибка получения координат:", error);
     //     });
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+  function getCurrentLocation() {
+        return new Promise((resolve, reject) => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => resolve([position.coords.latitude, position.coords.longitude]),
+                    error => reject(error)
+                );
+            } else {
+                reject("Геолокация не поддерживается этим браузером.");
+            }
+        });
+    }
+    
+    getCurrentLocation()
+        .then(coordinates => {
+            console.log("Координаты местоположения: "+coordinates);
+            
+            let map = L.map('map').setView(coordinates, 6); //По координатам  Перми
+            
+
+            const basemaps = {
+                StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: '&copy; OpenStreetMap'}),
+                Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
+                Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'}),
+            };
+            basemaps.StreetView.addTo(map);
+            L.control.layers(basemaps).addTo(map);
+          
+            const flagIcon = L.icon({
+                iconUrl: 'https://raw.githubusercontent.com/korostelv/image/main/flag.png',
+                iconSize: [40, 40],
+            });
+
+            L.marker(coordinates).bindPopup('Вы находитесь здесь.').addTo(map); //метка местоположения по IP
+        
+            for ( let i in cityDict) {
+                L.marker([cityDict[i]['lat'], cityDict[i]['long']], {icon: flagIcon}).bindPopup(i).addTo(map);
+            }
+
+        })
+
+        .catch(error => {
+            console.error("Ошибка получения координат:", error);
+            console.log("Координаты не найдены. Ориентировка по городу Пермь.")
+            let map = L.map('map').setView([58.00, 56.15], 6); // По координатам Перми
+            
+
+            const basemaps = {
+                StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: '&copy; OpenStreetMap'}),
+                Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
+                Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'}),
+            };
+            basemaps.StreetView.addTo(map);
+            L.control.layers(basemaps).addTo(map);
+          
+            const flagIcon = L.icon({
+                iconUrl: 'https://raw.githubusercontent.com/korostelv/image/main/flag.png',
+                iconSize: [40, 40],
+            });
+
+            // L.marker([58.00, 56.15]).bindPopup('Вы находитесь здесь.').addTo(map); //метка местоположения Перми
+            for ( let i in cityDict) {
+                L.marker([cityDict[i]['lat'], cityDict[i]['long']], {icon: flagIcon}).bindPopup(i).addTo(map);
+            }
+
+        });
+
+
+
+
 
 
     
